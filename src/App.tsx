f75283e2224dbb77ca4a152c4bda212c9bca2def
macOs <mail@mail.com>
2025-09-02 12:56:51 +0500

    add city input field

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,11 @@ import icon from './assets/react.svg'
 function App() {
   const [city, setCity] = useState<string>('Moscow');
   const [weather, setWeather] = useState<any>(null); // eslint-disable-line
+  const [forecast, setForecast] = useState<any[]>([]); //eslint-disable-line
+
+  const api_key = 'c96c6305fba278fca2a8d083855e9acb';
 
   const weatherFetch = () =>{
-    const api_key = 'c96c6305fba278fca2a8d083855e9acb';
-
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}&units=metric&lang=ru`)
       .then(response => response.json())
       .then(data =>{
@@ -20,13 +21,41 @@ function App() {
       });
 
   };
+
+  const forecastFetch = () => {
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${api_key}&units=metric&lang=ru`)
+      .then(response => response.json())
+      .then(data => { setForecast(data)})
+      .catch(error => {
+        console.error(error);
+      });
+  };
+
+  const handleCityChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setCity(e.target.value);
+  };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    weatherFetch();
+  }
+
   useEffect(() => {
     weatherFetch();
   }, [])
 
+  const getWeatherIconUrl = (iconId: string) => {
+    return `https://openweathermap.org/img/wn/${iconId}@2x.png`
+  }
+
   return (
     <>
     <div className='flex flex-col items-center'>
+      <form onSubmit={handleSubmit}>
+      <div className='border-3 rounded-lg bg-sky-300 border-green-900 font-bold'>
+        <input type="text" placeholder='Введите город' value={city} onChange={handleCityChange}/>
+      </div>
+      </form>
       <div className='flex justify-center mt-6'>
       <div className='shadow-md rounded-xl min-w-220 min-h-100 bg-green-700 gap-6'>
         {weather ?(
@@ -40,10 +69,10 @@ function App() {
             
         </div>
         <div className='flex justify-center'>
-          <img src={icon} alt="" className='pt-20' />
+          <img src={getWeatherIconUrl(weather.weather[0].icon)} alt="" className='pt-20' />
         </div>
         <div className='text-white pt-25 flex justify-between'>
-            <p className='font-bold pl-2 text-[25px]'>Ощущается как: {Math.round(weather.main.temp)} °C</p>
+            <p className='font-bold pl-2 text-[25px]'>Ощущается как: {Math.round(weather.main.feels_like)} °C</p>
             <p className='font-bold pr-2 text-[25px]'>Ветер: {Math.round(weather.wind.speed)} м/с </p>
         </div>
         </>
@@ -62,10 +91,42 @@ function App() {
                 <p className='text-white pl-2 pt-3'>День недели</p>
               </div>
             </div>
-            <div className='bg-green-700 shadow-md rounded-xs'>2</div>
-            <div className='bg-green-700 shadow-md rounded-xs'>3</div>
-            <div className='bg-green-700 shadow-md rounded-xs'>4</div>
-            <div className='bg-green-700 shadow-md rounded-xs'>5</div>
+            <div className='bg-green-700 shadow-md rounded-xs'>
+              <div className='flex justify-center mt-8'>
+                <img src={icon} alt="" />
+              </div>
+              <div className='font-bold'>
+                <p className='text-white pr-2 pt-3'>16 c</p>
+                <p className='text-white pl-2 pt-3'>День недели</p>
+              </div>
+            </div>
+            <div className='bg-green-700 shadow-md rounded-xs'>
+              <div className='flex justify-center mt-8'>
+                <img src={icon} alt="" />
+              </div>
+              <div className='font-bold'>
+                <p className='text-white pr-2 pt-3'>16 c</p>
+                <p className='text-white pl-2 pt-3'>День недели</p>
+              </div>
+            </div>
+            <div className='bg-green-700 shadow-md rounded-xs'>
+              <div className='flex justify-center mt-8'>
+                <img src={icon} alt="" />
+              </div>
+              <div className='font-bold'>
+                <p className='text-white pr-2 pt-3'>16 c</p>
+                <p className='text-white pl-2 pt-3'>День недели</p>
+              </div>
+            </div>
+            <div className='bg-green-700 shadow-md rounded-xs'>
+              <div className='flex justify-center mt-8'>
+                <img src={icon} alt="" />
+              </div>
+              <div className='font-bold'>
+                <p className='text-white pr-2 pt-3'>16 c</p>
+                <p className='text-white pl-2 pt-3'>День недели</p>
+              </div>
+            </div>
       </div>
     </div>
     </>
